1f4e1cdcbc4a082e28c65699ac838dd6b96d79e7
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-03 21:37:15 -0400

    New activity created color passing tested

--- app/src/main/java/edu/temple/colorpicker2/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorpicker2/CanvasActivity.java
@@ -1,14 +1,21 @@
+
 package edu.temple.colorpicker2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CanvasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView textView;
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_canvas);
+
+        textView = findViewById(R.id.textView2);
+        textView.setText(getIntent().getStringExtra("colorName"));
     }
 }
--- app/src/main/java/edu/temple/colorpicker2/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorpicker2/PaletteActivity.java
@@ -2,6 +2,7 @@ package edu.temple.colorpicker2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -18,25 +19,32 @@ public class PaletteActivity extends AppCompatActivity {
 
 
 
-        String[] colors = {"Choose a color", "Red", "Blue", "Green", "Magenta", "Purple", "Black", "Yellow", "White", "Cyan", "Grey"};
+        final String[] colors = {"Choose a color", "Red", "Blue", "Green", "Magenta", "Purple", "Black", "Yellow", "White", "Cyan", "Grey"};
 
         Spinner spinner = findViewById(R.id.spinner);
         ColorAdapter adapter = new ColorAdapter(this,colors);
         spinner.setAdapter(adapter);
+        spinner.setSelection(0,false);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                TextView textView = (TextView) view;
+                //TextView textView = (TextView) view;
+                //String colorValue = textView.getText().toString();
 
+                Intent intent = new Intent(PaletteActivity.this,CanvasActivity.class);
+                intent.putExtra("colorName",colors[position]);
+                startActivity(intent);
+
+                /*
                 try {
                     findViewById(R.id.backgroundID).setBackgroundColor(Color.parseColor(textView.getText().toString()));
                 }
                 catch (Exception e)
                 {
                     findViewById(R.id.backgroundID).setBackgroundColor(Color.WHITE);
-                }
+                }*/
             }
 
             @Override
