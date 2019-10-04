b493b93582ab73852845ba25beae7adbde106da5
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-03 21:57:11 -0400

    Colors displayed with text

--- app/src/main/java/edu/temple/colorpicker2/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorpicker2/CanvasActivity.java
@@ -3,6 +3,7 @@ package edu.temple.colorpicker2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,11 +12,22 @@ public class CanvasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         TextView textView;
+        String color = getIntent().getStringExtra("colorName");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
 
         textView = findViewById(R.id.textView2);
-        textView.setText(getIntent().getStringExtra("colorName"));
+        textView.setText(color);
+        textView.setTextSize(30);
+        textView.setTextColor(Color.BLACK);
+        if(color.equals("Black"))
+        {
+            textView.setTextColor(Color.WHITE);
+
+        }
+        findViewById(R.id.backgroundID).setBackgroundColor(Color.parseColor(color));
+
+
     }
 }
