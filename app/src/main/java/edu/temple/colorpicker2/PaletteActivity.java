34b7cddeccaa798fcc4e16f64b0ba75f71484f3a
MaxGoroshkoTU <tug47632@temple.edu>
2019-10-15 14:40:25 -0400

    New strings created

--- app/src/main/java/edu/temple/colorpicker2/PaletteActivity.java
+++ app/src/main/java/edu/temple/colorpicker2/PaletteActivity.java
@@ -3,6 +3,7 @@ package edu.temple.colorpicker2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -18,19 +19,22 @@ public class PaletteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] colors = {"Choose a color", "Red", "Blue", "Green", "Magenta", "Purple", "Black", "Yellow", "White", "Cyan", "Grey"};
+        //final String[] colors = {"Choose a color", "Red", "Blue", "Green", "Magenta", "Purple", "Black", "Yellow", "White", "Cyan", "Grey"};
+
+        Resources res = this.getResources();
+        final String [] gridlabels = res.getStringArray(R.array.colors);
 
         final Spinner spinner = findViewById(R.id.spinner);
-        ColorAdapter adapter = new ColorAdapter(this,colors);
+        ColorAdapter adapter = new ColorAdapter(this,gridlabels);
         spinner.setAdapter(adapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(!colors[position].equals("Choose a color")) {
+                if(!gridlabels[position].equals("Choose a color")) {
                     Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
-                    intent.putExtra("colorName", colors[position]);
+                    intent.putExtra("colorName", gridlabels[position]);
                     startActivity(intent);
                     spinner.setSelection(0,false);
                 }
